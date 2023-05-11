e36d559edcaae7533469e75ca4f1f2e4b5cd2701
Alena <pluwa@mail.ru>
2023-05-11 22:53:23 +0400

    build sucsess

--- src/test/java/DebitCardSeleniumTest.java
+++ src/test/java/DebitCardSeleniumTest.java
@@ -42,24 +42,17 @@ public class DebitCardSeleniumTest {
     @Test
     void shouldCardOrder() {
 
-        driver.findElement (By.cssSelector("[data-test-id=name] input")).sendKeys("Заглада Алёна");
-        driver.findElement (By.cssSelector("[data-test-id=phone] input")).sendKeys("+79608309609");
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Заглада Алена"); // с буквой Ё тест падает
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79608309609");
         driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
         driver.findElement(By.className("button__text")).click();
         String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        String actual = driver.findElement (By.cssSelector("[data-test-id=order-success]")).getText().trim();
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
         assertEquals(expected, actual);
     }
 
 
-//        List<WebElement> elements = driver.findElements(By.className("input__control"));
-//        elements.get(0).sendKeys("Заглада Алёна");
-//        elements.get(1).sendKeys("+79608309609");
-//        driver.findElement(By.className("checkbox__box")).click();
-//        driver.findElement(By.className("button")).click();
-//        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-//        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
-//        assertEquals(expected, actual);
+
 
 
 }
